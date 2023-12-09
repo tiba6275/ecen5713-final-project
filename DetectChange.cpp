8361159b67ddea29b041a36842fe1484c61fde68
tiba6275 <tiba6275@colorado.edu>
2023-12-09 04:49:24 -0500

    Modify DetectChange

--- DetectChange.cpp
+++ DetectChange.cpp
@@ -9,8 +9,21 @@
 void captureImage(const char* filename) {
     std::string command = "libcamera-jpeg -n -o " + std::string(filename) + " --width 1640 --height 1232";
     system(command.c_str());
-    command = "gst-launch-1.0 tcpserversrc host=192.168.1.116 port=9000 ! filesink location=" + std::string(filename);
-    system(command.c_str());
+}
+
+void sendImage(const std::string& filename, const std::string& serverIp, const std::string& port) {
+    std::string pipelineStr = "filesrc location=" + filename + " ! jpegenc ! tcpclientsink host=" + serverIp + " port=" + port;
+    GstElement* pipeline = gst_parse_launch(pipelineStr.c_str(), NULL);
+    gst_element_set_state(pipeline, GST_STATE_PLAYING);
+
+    GstBus* bus = gst_element_get_bus(pipeline);
+    GstMessage* msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE, static_cast<GstMessageType>(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
+
+    if (msg != NULL)
+        gst_message_unref(msg);
+    gst_object_unref(bus);
+    gst_element_set_state(pipeline, GST_STATE_NULL);
+    gst_object_unref(pipeline);
 }
 
 void compareImages(const char* in1, const char* in2, const std::string& out) {
@@ -33,10 +46,6 @@ void compareImages(const char* in1, const char* in2, const std::string& out) {
 }
 
 int main(int argc, char *argv[]) {
-    const char* inFile1 = "in1.jpg";
-    const char* inFile2 = "in2.jpg";
-    int outIdx = 0;
-
     gst_init(&argc, &argv);
 
     if (argc < 3) {
@@ -47,28 +56,23 @@ int main(int argc, char *argv[]) {
     std::string serverIp = argv[1];
     std::string port = argv[2];
 
+    const char* inFile1 = "in1.jpg";
+    const char* inFile2 = "in2.jpg";
+    int outIdx = 0;
+
     captureImage(inFile1);
+    sendImage(inFile1, serverIp, port);
 
     while (true) {
         captureImage(inFile2);
+        sendImage(inFile2, serverIp, port);
 
         std::string outFile = "out" + std::to_string(outIdx) + ".jpg";
 
         std::thread comparisonThread(compareImages, inFile1, inFile2, outFile);
         comparisonThread.join();
 
-        std::string pipelineStr = "filesrc location=" + outFile + " ! tcpclientsink host=" + serverIp + " port=" + port;
-        GstElement *pipeline = gst_parse_launch(pipelineStr.c_str(), NULL);
-        gst_element_set_state(pipeline, GST_STATE_PLAYING);
-
-        GstBus *bus = gst_element_get_bus(pipeline);
-        GstMessage *msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE, static_cast<GstMessageType>(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
-
-        if (msg != NULL)
-            gst_message_unref(msg);
-        gst_object_unref(bus);
-        gst_element_set_state(pipeline, GST_STATE_NULL);
-        gst_object_unref(pipeline);
+        sendImage(outFile, serverIp, port);
 
         outIdx++;
 
