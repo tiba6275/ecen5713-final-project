7265f168c61f55451732053b7e30b75abf353f53
tiba6275 <tiba6275@colorado.edu>
2023-12-09 04:39:47 -0500

    Fix a typo again

--- DetectChange.cpp
+++ DetectChange.cpp
@@ -9,7 +9,7 @@
 void captureImage(const char* filename) {
     std::string command = "libcamera-jpeg -n -o " + std::string(filename) + " --width 1640 --height 1232";
     system(command.c_str());
-    command = "gst-launch-1.0 tcpserversrc host=192.168.0.116 port=9000 ! filesink location=" + std::string(filename);
+    command = "gst-launch-1.0 tcpserversrc host=192.168.1.116 port=9000 ! filesink location=" + std::string(filename);
     system(command.c_str());
 }
 
