4029e6ec1597035bca94eb28299951f9d752aef8
tiba6275 <tiba6275@colorado.edu>
2023-12-08 22:54:43 -0500

    Include gstreamer & modify photo resolution

--- DetectChange.cpp
+++ DetectChange.cpp
@@ -4,9 +4,10 @@
 #include <unistd.h>
 #include <thread>
 #include <string>
+#include <gst/gst.h>
 
 void captureImage(const char* filename) {
-    std::string command = "libcamera-still -o " + std::string(filename);
+    std::string command = "libcamera-jpeg -n -o " + std::string(filename) + "--width 1640 --height 1232";
     system(command.c_str());
 }
 
@@ -29,22 +30,43 @@ void compareImages(const char* in1, const char* in2, const std::string& out) {
     cv::imwrite(out, diffImage);
 }
 
-int main() {
-    const char* inFile1 = "input1.jpg";
-    const char* inFile2 = "input2.jpg";
+int main(int argc, char *argv[]) {
+    const char* inFile1 = "in1.jpg";
+    const char* inFile2 = "in2.jpg";
     int outIdx = 0;
+
+    gst_init(&argc, &argv);
+
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " SERVER_IP PORT" << std::endl;
+        return 1;
+    }
+
+    std::string serverIp = argv[1];
+    std::string port = argv[2];
 
     captureImage(inFile1);
 
     while (true) {
-        sleep(5);
-
         captureImage(inFile2);
 
         std::string outFile = "out" + std::to_string(outIdx) + ".jpg";
 
         std::thread comparisonThread(compareImages, inFile1, inFile2, outFile);
         comparisonThread.join();
+
+        std::string pipelineStr = "filesrc location=" + outFile + " ! tcpserversink host=" + serverIp + "port=" + port;
+        GstElement *pipeline = gst_parse_launch(pipelineStr.c_str(), NULL);
+        gst_element_set_state(pipeline, GST_STATE_PLAYING);
+
+        GstBus *bus = gst_element_get_bus(pipeline);
+        GstMessage *msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE, static_cast<GstMessageType>(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
+
+        if (msg != NULL)
+            gst_message_unref(msg);
+        gst_object_unref(bus);
+        gst_element_set_state(pipeline, GST_STATE_NULL);
+        gst_object_unref(pipeline);
 
         outIdx++;
 
